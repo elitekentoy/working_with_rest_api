3f27918a8266158e1d77a955f0d6dcec39dc446a
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-10 15:58:36 +0800

    Add base widgets of Note_modify

--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -1,14 +1,34 @@
-
 import 'package:flutter/material.dart';
 
 class NoteModify extends StatelessWidget {
-  const NoteModify({ Key? key }) : super(key: key);
+  const NoteModify({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Create note')),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: <Widget>[
+            const TextField(
+              decoration: InputDecoration(hintText: 'Note title'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const TextField(
+              decoration: InputDecoration(hintText: 'Note content'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
