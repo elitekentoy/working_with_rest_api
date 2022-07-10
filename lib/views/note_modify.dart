18cb91cdc9f07e4a06d79719b0b5c136007e6fc2
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-10 15:47:47 +0800

    Create note_modify (initial)

---
+++ lib/views/note_modify.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class NoteModify extends StatelessWidget {
+  const NoteModify({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Create note')),
+      body: Container(),
+    );
+  }
+}
