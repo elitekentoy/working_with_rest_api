d92b844607f9bf1889e7d542168b42f0a27329a7
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-10 23:49:17 +0800

    Create NoteInsert Class

---
+++ lib/models/note_insert.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class NoteInsert{
+  String noteTitle;
+  String noteContent;
+
+  NoteInsert({
+    required this.noteTitle,
+    required this.noteContent,
+  });
+}
